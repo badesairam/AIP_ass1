af7cd5113c656612c56326660e17ebc9bbc6c7d2
badesairam <130050079@iitb.ac.in>
2017-02-01 00:19:24 +0530

    Add files via upload

---
+++ ista.m
@@ -0,0 +1,15 @@
+function theta = ista(y,lambda,err,x0,A)
+    %y is the given image
+    %err is for checking convergence
+    xcurr = x0;
+    xdiff = 1;
+    iter = 0;
+    alpha = max(eig(A'*A));
+    while iter < 5000 && xdiff > err
+        xprev = xcurr;
+        %soft threshold
+        xcurr = wthresh((xprev+(A'*(y-A*xprev))/alpha),'s',lambda/(2*alpha));
+        iter = iter + 1;
+        xdiff = norm(xcurr-xprev);
+    end
+    theta = xcurr;
